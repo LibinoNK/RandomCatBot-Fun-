b83546ef3fcf47263ccda8c0c02caa8c2f802901
Nikita <Libino123@yandex.ru>
2024-03-16 23:03:50 +0600

    Meme 'rybov prodaete' added

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
 import asyncio
 import os
-from pathlib import Path
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
-from random import randint
-from aiogram.types import FSInputFile
+from aiogram.enums import ParseMode
+from aiogram.types import BotCommandScopeAllPrivateChats
 from dotenv import find_dotenv, load_dotenv
+
+from handlers.admin_private import admin_router
 
 load_dotenv(find_dotenv())
 
-bot = Bot(token=os.getenv('TOKEN'))
+from handlers.user_private import user_private_router
+from handlers.user_group import user_group_router
+from common.bot_cmds_list import private
+
+ALLOWED_UPDATES = ['message', 'edited_message']
+
+bot = Bot(token=os.getenv('TOKEN'), parse_mode=ParseMode.HTML)
+bot.my_admins_list = []
 
 dp = Dispatcher()
 
-
-def choose_pic_Nikita_Igorevich():
-    return FSInputFile(f'Cats/{randint(1, len(list(Path("Cats").iterdir())))}.jpg')
-
-
-@dp.message(CommandStart())
-async def start_cmd_Nikita_Igorevich(message: types.Message):
-    await message.answer('Напиши мне "Хочу котика" и я покажу тебе котика!')
-
-
-@dp.message()
-async def answer_Nikita_Igorevich(message: types.Message, bot: Bot):
-    text: str | None = message.text
-    if text.lower() in ['привет', 'hi', 'hello']:
-        await message.answer('Здравствуй!')
-    elif text.lower() in ['пока', 'bue', 'до свидания']:
-        await message.answer('До встречи!')
-    elif text.lower() in ['Главный вопрос жизни, вселенной и вообще', 'смысл жизни']:
-        await message.answer('42')
-    elif text.lower() == 'хочу котика':
-        await bot.send_photo(chat_id=message.chat.id, photo=choose_pic_Nikita_Igorevich())
-    else:
-        await message.answer(message.text)
+dp.include_router(user_private_router)
+dp.include_router(user_group_router)
+dp.include_router(admin_router)
 
 
 async def main_Nikita_Igorevich():
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    # await bot.delete_my_commands(scope=BotCommandScopeAllPrivateChats()) - удаление команд
+    await bot.set_my_commands(commands=private, scope=BotCommandScopeAllPrivateChats())
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
 if __name__ == '__main__':
